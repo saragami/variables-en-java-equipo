d57029eac16d9fb62e43a15208c215cd9760b0f6
sara <saragaminop@gmail.com>
2024-01-31 11:03:51 -0600

    ...

---
+++ Variablles-equipo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author himml
+ *
+ */
+module variableHotelTransilvania {
+}
---
+++ Variablles-equipo/src/variableHotelTransilvania/VariableHotelTransilvania.java
@@ -0,0 +1,42 @@
+package variableHotelTransilvania;
+
+public class VariableHotelTransilvania {
+	package ejercicioHotel;
+
+	public class HotelTransylvania {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			//HOTEL TRANSILVANIA
+			
+					boolean statusHabitacion = false;
+					System.out.println("Habitación libre: " + statusHabitacion);
+					
+					byte ocupantesXHabitacion = 5;
+					System.out.println("Ocupantes: " + ocupantesXHabitacion);
+					
+					short cantidadHabitaciones = 50;
+					System.out.println("Cantidad de habitaciones: " + cantidadHabitaciones);
+					
+					int numeroPisos = 10;
+					System.out.println("Número de pisos: " + numeroPisos);
+					
+					long clientesAnuales = 100600;
+					System.out.println("Cantidad de clientes anuales: " + clientesAnuales);
+					
+					float precioXHabitacion = 1500.50f;
+					System.out.println("Precio de la habitción: " + precioXHabitacion);
+					
+					double gananciaAnual = 5500000.7d;
+					System.out.println("Ganancias anuales: " + gananciaAnual);
+					
+					
+
+		}
+
+	}
+	
+	
+	
+
+}
